f5cfdb7b4780124532ab994655724666e179ea56
Conner Chappell <conner.chappell123@gmail.com>
2021-03-27 10:38:38 -0600

    Workout history displayed

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,89 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="small-container">
+    <h1>Fitness Tracker</h1>
+
+    <div>
+      <h3>Date</h3>
+      <p>Please select the type of exercise you would like to add to your current workout</p>
+      <button>Strength</button>
+      <button>Endurance</button>
+      <h3>Current Workout</h3>
+    </div>
+
+    <workout-history 
+    :workouts="sessions" 
+    />
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WorkoutHistory from './components/WorkoutHistory.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WorkoutHistory
+  },
+  data() {
+    return {
+      sessions: [
+        {
+          id: 1,
+          date: "2021-03-10",
+          exercises: [
+            {
+              exerciseType: "strength",
+              exercise: "Dumbbell Curls",
+              sets: 3,
+              reps: 12,
+              weight: 25
+            },
+            {
+              exerciseType: "strength",
+              exercise: "Bench Press",
+              sets: 3,
+              reps: 10,
+              weight: 135
+            },
+          ],
+        },
+        {
+          id: 2,
+          date: "2021-03-08",
+          exercises: [
+            {
+              exerciseType: "endurance",
+              exercise: "Running",
+              distance: "2 miles",
+              duration: "20 minutes",
+            },
+            {
+              exerciseType: "endurance",
+              exercise: "Swimming",
+              distance: "10 laps",
+              duration: "15 minutes"
+            },
+          ],
+        },
+      ],
+      currentWorkout: {},
+      adding: false,
+    };
+  },
+  methods() {
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  button {
+    background: #009435;
+    border: 1px solid #009435;
+  }
+
+  .small-container {
+    max-width: 680px;
+  }
 </style>
---
+++ src/components/WorkoutHistory.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="workoutHistory">
+        <h3>Workout History</h3>
+
+        <div class="noWorkoutsDisplayed" v-if="workouts.length < 1">
+            <p>There are no workouts saved in your history</p>
+        </div>
+
+        <div class="historyContainer">
+            <ul v-for="session in workouts" :key="session.id">
+                <li class="workoutDate">{{session.date}}</li>
+
+                <div class="singleExercise">
+                    <template v-for="(stuff, index) in session.exercises.filter((item) => item.exerciseType === 'strength')">
+                        <li :key="`exercise-${index}`">Exercise: {{stuff.exercise}}</li>
+                        <li :key="`sets-${index}`">Sets: {{stuff.sets}}</li>
+                        <li :key="`reps-${index}`">Reps: {{stuff.reps}}</li>
+                        <li :key="`weight-${index}`">Weight: {{stuff.weight}}</li>
+                    </template>
+                </div>
+                
+                <div class="singleExercise">
+                    <template v-for="(stuff, index) in session.exercises.filter((item) => item.exerciseType === 'endurance')">
+                    <li :key="`exercise-${index}`">Exercise: {{stuff.exercise}}</li>
+                    <li :key="`distance-${index}`">Distance: {{stuff.distance}}</li>
+                    <li :key="`duration-${index}`">Duration: {{stuff.duration}}</li>
+                    </template>
+                </div>
+
+                <div class="editDelete">
+                    <button>Edit</button>
+                    <button>Delete</button>
+                </div>    
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Workout",
+    props: {
+        workouts: Array,
+    },
+    methods: {
+        getStrength: function (exercise) {
+            const details = exercise.filter(
+                (theExercise) => theExercise.exerciseType === "strength"
+            );
+            console.log("Details", details);
+        },
+        getEndurance: function (exercise) {
+            const details = exercise.filter(
+                (theExercise) => theExercise.exerciseType === "endurance"
+            );
+            console.log("Details", details);
+        },
+    },
+};
+</script>
+
+<style>
+    li {
+        list-style-type: none;
+    }
+</style>
